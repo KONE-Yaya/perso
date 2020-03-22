ba1509c58eb1f323d84ed10f605755d27c7b0ae3
KONE-Yaya <koneyayangolo@gmail.com>
2020-03-22 07:00:04 +0100

    gf

--- client/Application.java
+++ client/Application.java
@@ -8,7 +8,7 @@ public class Application {
 		ConfigReader configreader = new ConfigReader();
 		configreader.read();
 		System.out.println(configreader.getVal("nom"));
-		System.out.println(configreader.getVal("prenom"));
+		System.out.println(configreader.getVal("prenom")); 
 	}
 
 }
